7ceed19645f68c1c51ac74c7af5de92e85722ddd
Okoosi Emmanuel <145022337+emm-ok@users.noreply.github.com>
2025-12-31 01:22:30 +0100

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import { ArrowUp } from 'lucide-react'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center'>
+        <p className='textsm text-muted-foreground'>
+            &copy;   {new Date().getFullYear()} Emmy'sTech, All rights reserved 
+        </p>
+
+        <a href="#hero" className='p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transition-colors'>
+            Back to Top <ArrowUp size={20} />
+        </a>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,43 @@
+import { Moon, Sun } from 'lucide-react';
+import React, { useEffect, useState } from 'react'
+import { cn } from '../lib/utils';
+
+const ThemeToggle = () => {
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    if (typeof window === 'undefined') return false;
+    return localStorage.getItem('theme') === 'dark';
+  });
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [isDarkMode]);
+  const toggleTheme = () => {
+    if (isDarkMode) {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+      setIsDarkMode(false)
+    } else {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+      setIsDarkMode(true)
+    }
+  }
+  return (
+    <button onClick={toggleTheme} className={cn("cursor-pointer fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300", 
+      "focus:outline-hidden"
+    )}>
+      {isDarkMode ? (
+        <Sun className='h-6 w-6 text-yellow-300' />
+      ) : (
+        <Moon className='h-6 w-6 text-blue-900' />
+      )}
+    </button>
+  )
+}
+
+export default ThemeToggle  
